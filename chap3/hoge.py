7dd2b9da3b3f5d6a146736bf9d95f922d4eec36b
Reo nakamura <n23023@std.it-colleg.ac.jp>
2023-05-24 08:14:30 +0000

    chap4

---
+++ chap3/hoge.py
@@ -0,0 +1,6 @@
+a b = 2, 3
+if (a > b):
+    print(a)
+else:
+    print(b)
+result = a if (a > b)eles
---
+++ chap4/zhy8y4m.py
@@ -0,0 +1,12 @@
+def even_odd(x):
+    if x % 2 == 0:
+        print("偶数")
+    else:
+        print("奇数")
+
+
+
+
+even_odd(1)
+even_odd(2)
+even_odd(3)
